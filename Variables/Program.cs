b69fe6518f6775135c8b75b7a3e154a1ed594dbf
Grawsie <ryanhockey@live.ca>
2022-09-20 09:39:04 -0300

    Variables class 1

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -11,9 +11,49 @@ namespace Variables
         static void Main(string[] args)
         {
             Console.WriteLine("Variables");
+            Console.WriteLine();
             Console.WriteLine("By Ryan");
-            Console.ReadKey(true);
 
+
+            // variables
+            
+            //integers
+            int score; // declaration - declare "score" is an int
+            score = 0; // initialization - init. "score" to be 0.
+            int enemyPoints;
+            enemyPoints = 100;
+
+
+           Console.WriteLine("Score: " + score); // display score
+
+            score = score + enemyPoints;
+
+                Console.WriteLine("Score: " + score);
+
+
+
+            //floating points
+            //1.5
+            //100.43456754
+
+            float lapTime; // declaration
+            lapTime = 0.0f; //initialization
+            Console.WriteLine(lapTime);
+            lapTime = 65.357f;
+            Console.WriteLine(lapTime);
+
+
+
+
+            // string (text)
+
+            string title; // declaration
+            title = "Newbie";
+            Console.WriteLine(title);
+            title = "Pro";
+            Console.WriteLine(title);
+            Console.ReadKey(true);
+            
         }
     }
 }
